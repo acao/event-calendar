dd44df29e9fca7180052544b1b0cf1ed0c30b236
Rikki <rikki.schulte@gmail.com>
2020-06-14 08:15:24 -0400

    change title

--- appConfig.js
+++ appConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Community Event Calendar',
+  title: 'Cleveland Police Abolition Calendar',
   subTitle: 'The easiest option to share events!',
   formLink: 'https://forms.gle/Hay717tHccEQVmpE6',
   limitMonthInTheFuture: 3,
