59306f02154e747371129fc6ddad9ba34a31d797
Rikki <rikki.schulte@gmail.com>
2020-06-14 19:58:31 -0400

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,10 @@ import { Box, Text, Anchor } from 'grommet';
 
 const Footer = () => (
   <Box tag="footer" justify="between" direction="row" pad="medium">
-    <Text>
-      This site is powered by &nbsp;
-      <Anchor href="https://www.netlify.com/">
-        <b>Netlify</b>
-      </Anchor>
-      &nbsp; ❤
-    </Text>
+    <Text>Abolish the Police! &nbsp; ❤</Text>
     <Text>
       Developed by &nbsp;
-      <Anchor href="http://emasuriano.com/">Ema Suriano</Anchor>
+      <Anchor href="http://cdsdc.org/">CDSDC</Anchor>
     </Text>
   </Box>
 );
