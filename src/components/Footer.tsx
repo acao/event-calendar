13ba80ebcd458dac7cdd12db564c0243f27b7def
Ryan Walker <ryanwalker.design@gmail.com>
2020-06-21 00:56:05 -0400

    made design more responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,41 @@
-import React from 'react';
-import { Box, Text, Anchor, Footer as GrommetFooter } from 'grommet';
+import React, { useContext } from 'react';
+import {
+  Box,
+  Text,
+  Anchor,
+  Footer as GrommetFooter,
+  ResponsiveContext,
+} from 'grommet';
 import styled from 'styled-components';
 
-const Footer = () => (
-  <GrommetFooter
-    background="footer-background"
-    tag="footer"
-    justify="between"
-    direction="row"
-    pad="small"
-  >
-    <Text size="small" weight="bold">
-      Abolish the Police! &nbsp; ❤
-    </Text>
-    <Text size="small" weight="bold">
-      Developed by &nbsp;
-      <StyledAnchor
-        color="footer-developer-link-color"
-        target="_blank"
-        href="http://cdsdc.org/"
-      >
-        CDSDC
-      </StyledAnchor>
-    </Text>
-  </GrommetFooter>
-);
+const Footer = () => {
+  const size = useContext(ResponsiveContext);
+  const isPhone = size === 'small';
+
+  return (
+    <GrommetFooter
+      background="footer-background"
+      tag="footer"
+      justify="between"
+      direction={isPhone ? 'column' : 'row'}
+      pad="small"
+    >
+      <Text size="small" weight="bold">
+        Abolish the Police! &nbsp; ❤
+      </Text>
+      <Text size="small" weight="bold">
+        Developed by &nbsp;
+        <StyledAnchor
+          color="footer-developer-link-color"
+          target="_blank"
+          href="http://cdsdc.org/"
+        >
+          CDSDC
+        </StyledAnchor>
+      </Text>
+    </GrommetFooter>
+  );
+};
 
 const StyledAnchor = styled(Anchor)`
   border-bottom: 2px solid tomato;
